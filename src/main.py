27be445458efde5a89de14e9cd939b6004dc3e29
Emmanuel M <emmanuelbendavid@gmail.com>
2019-07-29 17:15:33 -0300

    Started model info

--- src/main.py
+++ src/main.py
@@ -1,2 +1,24 @@
+import torch
+
+from torchtext import data
+from torchtext import datasets
+
+from  util import get_args
+
 if __name__ == '__main__':
-    print("main")
+    args = get_args()
+    if torch.cuda.is_available():
+        torch.cuda.set_device(args.gpu)
+        device = torch.device('cuda:{}'.format(args.gpu))
+        print("With GPU")
+    else:
+        device = torch.device('cpu')
+        print("With CPU")
+
+    inputs = data.Field(lower=args.lower)
+    answers = data.Field(sequential=False)
+
+    train, validation, test = datasets.SNLI.splits(inputs, answers)
+
+    
+
